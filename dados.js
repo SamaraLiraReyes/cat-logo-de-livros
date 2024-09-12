7a7ce6c52edea05169713d0fc063c2bf9696e19f
SamaraLiraReyes <samaraservicos@gmail.com>
2024-09-12 13:47:19 -0500

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : 'O Estrangeiro',
+        descricao : "O Estrangeiro de Albert Camus é uma obra que nos convida a uma profunda reflexão sobre a existência humana e o absurdo da vida.",
+        link : "https://pt.wikipedia.org/wiki/O_Estrangeiro_(livro)",
+        tags: "Albert Camus obra"
+    },
+
+    {
+        titulo: 'Em busca do tempo perdido',
+        descricao: "Em busca do tempo perdido, de Marcel Proust, é uma vasta obra que explora a memória, a consciência e a passagem do tempo, através das experiências de um narrador.",
+        link: "https://pt.wikipedia.org/wiki/Em_Busca_do_Tempo_Perdido",
+tags: "melhor livro seculo"   
+    },
+    {
+        titulo: 'O Processo',
+        descricao: "O Processo, de Franz Kafka, é um romance alegórico que narra a história de um homem acusado de um crime indefinido e submetido a um julgamento absurdo.",
+        link: "https://pt.wikipedia.org/wiki/O_Processo",
+    tags: "livros extraordinarios"
+    }
+];
